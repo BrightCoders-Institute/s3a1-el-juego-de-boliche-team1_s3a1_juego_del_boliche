effe240e85062f48e02a81df85fbf3753a706131
Alan Hernández <ahernandez102@ucol.mx>
2024-01-10 10:08:57 -0600

    Update class player, pin & score. Also added comments to the methods
    Co-authored-by: Fernando Arellano <farellano.0709@gmail.com>
    Co-authored-by: Luis Gutierrez <luisgutierrez26489@gmail.com>
    Co-authored-by: Yhoselin Sánchez <yossi2075.ys@gmail.com>

--- game.rb
+++ game.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-# Clase para definir al jugador
+# Clase para definir al jugador, se ingresa un nombre como parametro.
 class Player
   attr_accessor :name
 
@@ -8,44 +8,65 @@ class Player
     @name = name
   end
 
-  def throw(pines)
-    fall = rand(0..pines)
+  # El método roll permite al objeto jugador obtener un número entre 0 y 10,
+  # respresentando a los pinos que derriba.
+  def roll
+    rand(0..pines) # Se genera un número aleatorio entre 0 y 10.
   end
 end
 
-# Clase para definir los pinos
-class Pine
+# Clase para definir los pinos. Automáticamente se inicializan con 10 pinos.
+class Pin
   attr_accessor :quantity
 
   def initialize
     @quantity = 10
   end
 
-  def show_pines
+  # Nos muestra la cantidad de pinos actuales de la ronda.
+  def show_pins
     @quantity
   end
 
-  def update_pines(pines)
-    @quantity -= pines
-    puts "#{@quantity}"
+  # Resta la cantidad de pinos que el jugador derribo.
+  def update_pins(pins_knocked_down)
+    @quantity -= pins_knocked_down
+    puts "Pinos restantes: #{@quantity}"
   end
-  def restart_pines
+
+  # Reinicia el valor inicial de los pinos al inicio de cada ronda del jugador.
+  def reset_pins
     @quantity = 10
   end
 end
 
-# ! Clase para algoritmo de puntaje puntaje
+# Clase para añadir los puntajes del jugador en caso de hacer un strike o spare,
+# además de calcular su puntaje final.
 class Score
-  attr_accessor :score 
-  
-  def initialize 
+  attr_accessor :score
+
+  # Cada jugador incia con un puntaje de 0 y con un array que guardará sus spares o strikes.
+  def initialize
     @score = 0
+    @bonuses = []
   end
 
-  def spare
+  # Permite obtener el puntaje final de cada jugador.
+  def calculate_score
+    @score = 0
+    @bonuses.each { |bonus| @score += bonus } # Se suma cada elemento del array "bonuses" a la variable score.
+    puts "Puntaje total: #{@score}"
   end
 
-  def strike
+  # Se añaden los spare al array bonuses.
+  def spare(pins_knocked_down)
+    @bonuses << pins_knocked_down
+  end
+
+  # Se ingresan los parametros correspondientes al tiro 1 y 2 del jugador, para posteriormente sumarse
+  # y añadir el nuevo valor al array.
+  def strike(roll1, roll2)
+    @bonuses << roll1 + roll2
   end
 end
 
@@ -63,4 +84,3 @@ pine = Pine.new
 roll = player.throw(pine.show_pines)
 puts roll
 pine.update_pines(roll)
-
